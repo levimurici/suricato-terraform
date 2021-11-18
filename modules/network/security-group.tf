1c83c3871da11aa00a5e4ce65bd2f6fb0b879229
levimurici <levi.murici@gmail.com>
2021-11-18 15:02:03 -0300

    Estruturando instâncias e rede na AWS

--- modules/network/security-group.tf
+++ modules/network/security-group.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "acesso-dnd" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = "177.50.229.182"
+    cidr_blocks = ["177.50.229.182"]
   }
 
   egress  {
