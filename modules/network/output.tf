69ce10f869c60d64edaad2791582b1dcca1edf97
levimurici <levi.murici@gmail.com>
2021-11-18 14:49:24 -0300

    Estruturando instâncias e rede na AWS

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -3,22 +3,19 @@ output "aws_vpc" {
 }
 
 output "aws_subnet_private-1a" {
-  value = "${aws_subnet.production_private-1a}"
+  value = "${aws_subnet.dnd_private-1a}"
 
 }
 
 output "aws_subnet_private-1b" {
-  value = "${aws_subnet.production_private-1b}"
+  value = "${aws_subnet.dnd_private-1b}"
 
 }
 output "aws_subnet_public-1e" {
-  value = "${aws_subnet.production_public-1e}"
+  value = "${aws_subnet.dnd_public-1e}"
 
 }
 output "aws_subnet_public-1f" {
-  value = "${aws_subnet.production_public-1f}"
+  value = "${aws_subnet.dnd_public-1f}"
 
-}
-output "aws_subnet_database" {
-  value = "${aws_subnet.production_private-database-1d}"
 }
--- modules/network/public_subnet.tf
+++ modules/network/public_subnet.tf
@@ -24,11 +24,11 @@ resource "aws_subnet" "dnd_public-1f" {
   }
 }
 resource "aws_route_table_association" "public_1e" {
-  subnet_id      = "${aws_subnet.production_public-1e.id}"
+  subnet_id      = "${aws_subnet.dnd_public-1e.id}"
   route_table_id = "${aws_route_table.internet_gateway_route_table.id}"
 }
 
 resource "aws_route_table_association" "public_1f" {
-  subnet_id      = "${aws_subnet.production_public-1f.id}"
+  subnet_id      = "${aws_subnet.dnd_public-1f.id}"
   route_table_id = "${aws_route_table.internet_gateway_route_table.id}"
 }
