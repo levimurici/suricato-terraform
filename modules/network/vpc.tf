2dae21c350803a903c1f85280104e3110ceba604
levimurici <levi.murici@gmail.com>
2021-11-19 13:50:18 -0300

    Estruturando instâncias e rede na AWS

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc" "dnd_vpc" {
+  default_security_group_id = "${aws_security_group.acesso-dnd.id}"
   cidr_block           = "${var.cidr_block}"
   instance_tenancy     = "default"
   enable_dns_hostnames = true
