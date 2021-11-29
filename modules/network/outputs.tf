187fee7e892e04996a5aa3f91c31e945c569a4a8
Levi Murici <levi.murici@gmail.com>
2021-11-29 10:41:06 -0300

    Estruturando infra para suricato

---
+++ modules/network/outputs.tf
@@ -0,0 +1,37 @@
+/* output "aws_vpc" {
+  value = "${aws_vpc.dnd_vpc.id}"
+}
+
+output "aws_subnet_private-1a" {
+  value = "${aws_subnet.dnd_private-1a}"
+
+}
+
+output "aws_subnet_private-1b" {
+  value = "${aws_subnet.dnd_private-1b}"
+
+}
+output "aws_subnet_public-1e" {
+  value = "${aws_subnet.dnd_public-1e}"
+
+}
+output "aws_subnet_public-1f" {
+  value = "${aws_subnet.dnd_public-1f}"
+
+} */
+
+output "services_ip" {
+  value = "${aws_instance.services.public_ip}"
+}
+
+output "services_public_dns" {
+  value = "${aws_instance.services.public_dns}"
+}
+
+output "bots_ip" {
+  value = "${aws_instance.suricato-bots.public_ip}"
+}
+
+output "bots_public_dns" {
+  value = "${aws_instance.suricato-bots.public_dns}"
+}
---
+++ modules/network/security_group.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "acesso-dnd" {
+  name        = "acesso-dnd"
+  description = "Access SG VPN"
+  vpc_id      = "${aws_vpc.dnd_vpc.id}"
+
+  ingress {
+    description = "Access Ec2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = "${var.cidr_security_group}"
+  }
+
+  ingress {
+    description = "Acesso do lab"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["177.50.229.182/32"]
+  }
+
+  ingress {
+    description = "ping"
+    from_port   = 20
+    to_port     = 20
+    protocol    = "ICMP"
+    cidr_blocks = "${var.cidr_security_group}"
+  }
+
+  egress  {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "dnd default vpc"
+    Iac     = "Terraform"
+    Service = "Security Group"
+  }
+}
